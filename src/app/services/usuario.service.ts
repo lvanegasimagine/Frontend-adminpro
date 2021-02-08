56ec09358e79ef8cfa62396fc62d3d23f1126063
Luis Vanegas <lvanegas1429@gmail.com>
2021-02-08 15:23:43 -0600

    Visualización de Avatar Usuario.model modificado

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -124,6 +124,12 @@ export class UsuarioService {
 
   cargarUsuario(desde: number = 0) {
     const url = `${base_url}/usuarios?desde=${desde}`;
-    return this.http.get<CargarUsuario>(url, this.headers);
+    return this.http.get<CargarUsuario>(url, this.headers).pipe(map(resp => {
+      const usuarios = resp.usuarios.map(user => new Usuario(user.nombre, user.email, '', user.img, user.google, user.role, user.uid));
+      return {
+        total: resp.total,
+        usuarios
+      };
+    }));
   }
 }
