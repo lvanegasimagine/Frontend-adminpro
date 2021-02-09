9f92b730c498877ba9019e3f3c3705b0f312a91e
Luis Vanegas <lvanegas1429@gmail.com>
2021-02-09 14:40:58 -0600

    CRUD Hospital En servicios y agregación de boton

--- src/app/services/hospital.service.ts
+++ src/app/services/hospital.service.ts
@@ -23,11 +23,26 @@ export class HospitalService {
       headers: {
         'x-token': this.token
       }
-    }
+    };
   }
 
   cargarHospital() {
     const url = `${base_url}/hospitales`;
     return this.http.get(url, this.headers).pipe(map((resp: {ok: boolean, hospitales: Hospital[]}) => resp.hospitales));
+  }
+
+  crearHospital(nombre: string) {
+    const url = `${base_url}/hospitales`;
+    return this.http.post(url, {nombre}, this.headers);
+  }
+
+  actualizarHospital(_id: string, nombre: string) {
+    const url = `${base_url}/hospitales/${_id}`;
+    return this.http.put(url, {nombre}, this.headers);
+  }
+
+  eliminarHospital(_id: string) {
+    const url = `${base_url}/hospitales/${_id}`;
+    return this.http.delete(url, this.headers);
   }
 }
