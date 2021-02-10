abd5efd9d53665aa2a0a02bc9bb82673de487eb5
Luis Vanegas <lvanegas1429@gmail.com>
2021-02-10 13:10:32 -0600

    mostrar imagenes de los hospitales dinamicamentes

--- src/app/pages/mantenimientos/medicos/medico.component.ts
+++ src/app/pages/mantenimientos/medicos/medico.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MedicoService } from '../../../services/medico.service';
 import { Medico } from '../../../models/medicos.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HospitalService } from 'src/app/services/hospital.service';
+import { Hospital } from '../../../models/hospital.model';
 
 @Component({
   selector: 'app-medico',
@@ -10,21 +13,31 @@ import { Medico } from '../../../models/medicos.model';
 })
 export class MedicoComponent implements OnInit {
 
-  public medicos: Medico[] = [];
-  public cargando: boolean = true;
+  public medicoForm: FormGroup;
+  public hospitales: Hospital[] = [];
+  public hospitalSeleccionado: Hospital;
 
-  constructor(private medicoService: MedicoService) { }
+  constructor(private hospitalService: HospitalService, private fb:FormBuilder) { }
 
   ngOnInit(): void {
-  }
-
-  cargarHospital() {
-    this.cargando = true;
-    this.medicoService.cargarMedicos().subscribe((medicos: Medico[]) => {
-      this.cargando = false;
-      this.medicos = medicos;
-      console.log(medicos);
+    this.medicoForm = this.fb.group({
+      nombre: ['Hernando', Validators.required],
+      hospital: ['', Validators.required],
+    });
+    this.cargarHospitales();
+    this.medicoForm.get('hospital').valueChanges.subscribe(hospitalID => {
+      this.hospitalSeleccionado = this.hospitales.find(h => h._id === hospitalID);
     });
   }
 
+  cargarHospitales() {
+    this.hospitalService.cargarHospital().subscribe((resp: Hospital[]) => {
+      this.hospitales = resp;
+      console.log('hospitales', resp);
+    });
+  }
+
+  guardarMedico() {
+    console.log(this.medicoForm.value);
+  }
 }
